eba214b1c1ca58236fdb66ddc06be3b2f70ff79d
Keith Gaughan <keith@talideon.com>
2008-05-31 15:50:25 +0100

    Added my netstring reader.

---
+++ netstrings.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+class MalformedNetstringError:
+    pass
+
+def netstring_reader(fd):
+    while True:
+        n = ""
+        while True:
+            c = fd.read(1)
+            if c == '':
+                return
+            if c == ':':
+                break
+            if len(n) > 10:
+                raise MalformedNetstringError
+            if '0' > c > '9':
+                # Must be made up of digits.
+                raise MalformedNetstringError
+            if c == '0' and n == '':
+                # We can't allow leading zeros.
+                if fd.read(1) != ':':
+                    raise MalformedNetstringError
+                n = c
+                break
+            n += c
+        n = int(n, 10)
+        payload = fd.read(n)
+        if len(payload) < n:
+            return
+        if fd.read(1) != ',':
+            raise MalformedNetstringError
+        yield payload
+
+fd = open("sample.dat", "r")
+try:
+    for i in netstring_reader(fd):
+        print i
+finally:
+    fd.close()
