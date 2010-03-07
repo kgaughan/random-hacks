6a9a8f80131f7f0899877425b2a9e64c6515a196
Keith Gaughan <keith@talideon.com>
2010-03-07 04:29:39 +0000

    Added a proper test to the netstring library, cleaned it up a bit, and made pylint happy.

--- netstrings.py
+++ netstrings.py
@@ -1,42 +1,77 @@
-#!/usr/bin/env python
+#
+# Copyright (c) Keith Gaughan, 2009.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted, provided
+# that the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation, and that the name of the author not be used in advertising or
+# publicity pertaining to distribution of the software without specific,
+# written prior permission.
+#
+# THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+# ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+# VINAY SAJIP BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+# ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
-class MalformedNetstringError:
+"""
+A simple module for reading netstrings from a file object.
+
+Copyright (c) Keith Gaughan, 2009. See the source code for the full licence.
+"""
+
+
+class MalformedNetstringError(Exception):
+    """Raised if netstring_reader() hits something it can't consume."""
     pass
 
-def netstring_reader(fd):
+def netstring_reader(fdesc):
+    """
+    Wraps a file object in a generator that yields netstrings parsed from
+    its contents.
+
+    >>> import StringIO
+    >>> fd = StringIO.StringIO('13:hello, world!,0:,12:how are you?,')
+    >>> list(netstring_reader(fd))
+    ['hello, world!', '', 'how are you?']
+    >>> fd.close()
+    """
     while True:
-        n = ""
+        nch = ""
         while True:
-            c = fd.read(1)
-            if c == '':
+            char = fdesc.read(1)
+            if char == '':
                 return
-            if c == ':':
+            if char == ':':
                 break
-            if len(n) > 10:
-                raise MalformedNetstringError
-            if '0' > c > '9':
+            if len(nch) > 10:
+                raise MalformedNetstringError, "Excessive length"
+            if '0' > char > '9':
                 # Must be made up of digits.
-                raise MalformedNetstringError
-            if c == '0' and n == '':
+                raise MalformedNetstringError, "Bad length digit"
+            if char == '0' and nch == '':
                 # We can't allow leading zeros.
-                if fd.read(1) != ':':
-                    raise MalformedNetstringError
-                n = c
-                break
-            n += c
-        n = int(n, 10)
-        payload = fd.read(n)
-        if len(payload) < n:
+                if fdesc.read(1) != ':':
+                    raise MalformedNetstringError, "No leading zeros"
+            nch += char
+        nch = int(nch, 10)
+        payload = fdesc.read(nch)
+        if len(payload) < nch:
             return
-        if fd.read(1) != ',':
-            raise MalformedNetstringError
+        terminator = fdesc.read(1)
+        if terminator != ',':
+            raise MalformedNetstringError, "Bad terminator: '%s'" % terminator
         yield payload
 
 
+def _test():
+    """Run doctests on this module."""
+    import doctest
+    doctest.testmod()
+
+
 if __name__ == '__main__':
-    fd = open("sample.dat", "r")
-    try:
-        for i in netstring_reader(fd):
-            print i
-    finally:
-        fd.close()
+    _test()
